228ae22f388ad5e4d92b06738387cb642fd23d65
javaMercy <nextelonmusk01@gmail.com>
2022-06-29 17:15:18 +0300

    Users, customers, and rentals tables have been added to the project.

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -10,9 +10,45 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
+            //CarDetailsTest();
 
+            //RentalTest();
+
+            //UserTest();s
+
+            //CustomerTest();
+        }
+
+        private static void CustomerTest()
+        {
+            CustomerManager customerManager = new CustomerManager(new EfCustomerDal());
+
+            customerManager.Add(new Customer { UserId = 2, CompanyName = "SolidTeam" });
+        }
+
+        private static void UserTest()
+        {
+            UserManager userManager = new UserManager(new EfUserDal());
+
+            userManager.Add(new User { FirstName = "Engin", LastName = "Demiroğ", Email = "Engindemiroğ06@gmail.com", Password = "217645" });
+            userManager.Add(new User { FirstName = "Mustafa Murat", LastName = "Coşkun", Email = "Mustafacoşkun34@gmail.com", Password = "923121" });
+        }
+
+        private static void RentalTest()
+        {
+            RentalManager rentalManager = new RentalManager(new EfRentalDal());
+
+            var result = rentalManager.GetAll();
+
+            foreach (var rental in result.Data)
+            {
+                Console.WriteLine("{0} - {1} - {2}", rental.CarId, rental.CustomerId, rental.RentDate);
+            }
+        }
+
+        private static void CarDetailsTest()
+        {
             CarManager carManager = new CarManager(new EfCarDal());
-
             var result = carManager.GetCarDetails();
 
             foreach (var carDetail in result.Data)
@@ -25,7 +61,6 @@ namespace ConsoleUI
                 }
 
             }
-
         }
     }
 }
